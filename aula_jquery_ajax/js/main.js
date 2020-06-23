42c34ba46998ada17ce48082817ddf08662e3585
Mariana <mxpixel@yahoo.com.br>
2020-06-23 11:26:36 -0300

    Parte 2 - Desenvolvendo jQuery com Bootstrap

--- aula_jquery_ajax/js/main.js
+++ aula_jquery_ajax/js/main.js
@@ -1,6 +1,11 @@
 function consultaCep(){
+    
+    $(".cep").hide();
+    $(".barra-progesso").show();
+
     var cep = document.getElementById("cep").value;
     var url= "https://viacep.com.br/ws/" + cep + "/json/";
+
     $.ajax({
         url: url,
         type: "GET",
@@ -8,11 +13,27 @@ function consultaCep(){
             console.log(response);
             //$ pra acessar o jquery
             //# = id // . = classe
+            
+            $("#titulo_cep").html("CEP: "+cep);
             $("#logradouro").html(response.logradouro); // outra forma
-            //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+
+            $(".cep").show();
+            $(".barra-progesso").hide();
+        
+        },
+        error: function(response){
+
+            $("#erro-cep").html("CEP nao encontrado!");
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progesso").hide();
+});
+
+//uma vantagem ajax é q tras os dados sem recarregar a pagina
