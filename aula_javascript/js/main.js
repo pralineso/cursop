fab37dbfaae49aa0ff03e1326af1944caa825665
Mariana <mxpixel@yahoo.com.br>
2020-06-23 19:33:19 -0300

    Parte 2 - Desenvolva paginas web com JavaScript

--- aula_javascript/js/main.js
+++ aula_javascript/js/main.js
@@ -1,3 +1,59 @@
+//Funçoes
+
+function botao(){
+    document.getElementById("mensagem").innerHTML = "<i>Bem Vind@ !</i>";
+    //alert("Bem Vind@ !!!");
+}
+
+function redirecionar(){
+    window.open("https://google.com/"); // chama nova janela
+    //window.location.href = "https://github.com/"; // abre na msm janela
+}
+
+function trocar(elemento){
+   // document.getElementById("mouseover").innerHTML = "Texto modificao ao passar o mouse";
+    elemento.innerHTML = "Texto modificao ao passar o mouse";
+    //alert("Passou o mouse");
+}
+
+function voltar(elemento){
+    //document.getElementById("mouseover").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina Carregada!")
+}
+
+function change(elemento){
+    console.log(elemento.value);
+}
+
+function soma (n1, n2){
+    return n1+n2;
+}
+
+function setReplace (frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }else {
+        validar = false;
+    }
+    return validar;
+}
+
+   // var idade = prompt("Qual sua idade?");
+    //console.log(validaIdade(idade));
+
+    //alert(soma(5, 10));
+    //alert(setReplace("Amo café", "café", "chocolate"));
+
+/**
 //variaveis
     var nome = "Mariana";
     var idade = 25;
@@ -42,14 +98,14 @@
     console.log(frutas[1].cor);
 
 //Condições
-    /**
+
      idade =  prompt("Qual sua idade?");
     if(idade >= 18){
         alert("Maior de idade");
     }else {
         alert("Menor de idade");
     }
-    */
+
 
  //Laços de repetiçao
     var count = 0;
@@ -58,11 +114,11 @@
         count++; 
     };
 
-    /**
+
     for(count = 0; count <=5; count++){
         alert(count);
     };
-    */
+
 
 //Datas
     var data = new Date();
@@ -72,3 +128,4 @@
     console.log(data.getMinutes());
     console.log(data.getMonth()+1);//começa em 0
 
+*/
