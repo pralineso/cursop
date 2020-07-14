fce3f86b77e5afbbfd535c77dc1a5a5c1bb7b174
Mariana <mxpixel@yahoo.com.br>
2020-07-14 16:25:17 -0300

    Desenvolvimento para Internet e Banco de Dados com Python com Django - Parte 4.2 (create e gitignore)

--- aula_python/python_django/agenda2/agenda2/urls.py
+++ aula_python/python_django/agenda2/agenda2/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     # path('', views.index),
     path('', RedirectView.as_view(url='/agenda/')),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user)
