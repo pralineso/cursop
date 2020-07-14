b788627e16320531bc5c41d294e2148942af7adc
Mariana <mxpixel@yahoo.com.br>
2020-07-13 22:07:06 -0300

    Desenvolvimento para Internet e Banco de Dados com Python com Django - Parte 2.3 (salvando aula porque o cod esta errado)

--- aula_python/python_django/agenda/core/models.py
+++ aula_python/python_django/agenda/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#aki é onde cria as tabelas
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento') #esse verbose é o nome q vai aparecer la na coluna
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #aki ta dando nome dessa tabela
+    class Meta:
+        db_table = 'eventos'
+
+    #aki ta tratando o objeto
+    def __str__(self):
+        return self.titulo
