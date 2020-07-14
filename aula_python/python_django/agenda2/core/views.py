5d5a2384e7f335e70ddb981aed7c5a4f6b51a105
Mariana <mxpixel@yahoo.com.br>
2020-07-14 10:40:58 -0300

    Desenvolvimento para Internet e Banco de Dados com Python com Django - Parte 3

--- aula_python/python_django/agenda2/core/views.py
+++ aula_python/python_django/agenda2/core/views.py
@@ -1,3 +1,15 @@
+# from django.shortcuts import render, redirect
 from django.shortcuts import render
-
+from core.models import Evento
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    # evento = Evento.objects.get(id=1) #so 1 user em especifico
+    # usuario = request.user #usuario autenticando
+    # evento = Evento.objects.filter(usuario=usuario) #usuario autenticado
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
