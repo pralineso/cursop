e6c2af24835e41ea2ff42193de1ba1b307667b67
Mariana <mxpixel@yahoo.com.br>
2020-07-15 14:57:09 -0300

    Desenvolvimento avançado Python com Flask e REST API - Ola Mundo!

---
+++ aula_python/python_flask/ola_flask/run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/<int:numero>", methods=['GET','POST'])
+def ola(numero):
+    return  'Ola mundo! {}'.format(numero)
+
+if __name__ == "__main__":
+    app.run(debug=True) #em modeo de producao o debug deve ficar falso
