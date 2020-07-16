7d205acf22812725fac0c1c4ed40c7b3944e8fb9
Mariana <mxpixel@yahoo.com.br>
2020-07-16 20:23:00 -0300

    Desenvolvimento avancado Python com Flask e REST API - Parte 5.2 (api com autenticacao)

--- aula_python/python_flask/api_atividades/app.py
+++ aula_python/python_flask/api_atividades/app.py
@@ -1,11 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from models import Pessoas, Atividades
+from models import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+# USUARIOS = {
+#     'Mariana Xavier':'123',
+#     'julia':'321'
+# }
+
+# @auth.verify_password #esse decorador é responsavels pro verificar a senha
+# def verificacao(login, senha):
+#     if not (login, senha):
+#         return False
+#     return  USUARIOS.get(login) == senha
+
+@auth.verify_password #esse decorador é responsavels pro verificar a senha
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
+
+
 class Pessoa(Resource):
+    @auth.login_required #esse decorador diz q para acessar esse metodo, o usuario precisa estar autenticado
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -44,6 +65,7 @@ class Pessoa(Resource):
         return {'status':'sucesso', 'mensagem':mensagem}
 
 class ListaPessoa(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id':i.id, 'nome':i.nome, 'idade':i.idade} for i in pessoas]
--- aula_python/python_flask/api_atividades/utils.py
+++ aula_python/python_flask/api_atividades/utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 #Insere dados na tabela pessoa
 def insere_pessoas():
@@ -28,9 +28,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Julia').first()
     pessoa.delete()
 
+def insere_usuarios(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
 
 if __name__ == '__main__':
+     insere_usuarios('mariana', '1234')
+     insere_usuarios('julia', '3214')
+     consulta_todos_usuarios()
      #insere_pessoas()
      # altera_pessoa()
      #exclui_pessoa()
-     consulta_pessoas()
+     #consulta_pessoas()
